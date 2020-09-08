876eff0e112a1588f3c0176766a130fc8c98962f
Arkadiy <arkadiy.and@gmail.com>
2020-09-08 18:19:35 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Enter a number");
+                } while (!int.TryParse(Console.ReadLine(), out n));
+
+                Console.WriteLine("Enter ESC to exit programm...");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape); 
+        }
+    }
+}
